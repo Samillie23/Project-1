ddec566e796148ffe92182485159b71b4d025467
Samillie23 <anisferia2023@gmail.com>
2025-09-05 17:12:00 -0700

    Terrain, Castle and Water
    Added Terrain, Castle and Water

--- Assets/Robot/Scripts/MovementInput.cs
+++ Assets/Robot/Scripts/MovementInput.cs
@@ -59,7 +59,6 @@ public class MovementInput : MonoBehaviour {
         moveVector = new Vector3(0, verticalVel * .2f * Time.deltaTime, 0);
         controller.Move(moveVector);
 
-
     }
 
     void PlayerMoveAndRotation() {
